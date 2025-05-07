49fc7e4f20d9c790010b65232f2ad622d4ad61d9
Volodymy Rudyk <rudykvova@gmail.com>
2025-05-07 16:13:54 +0300

    Delate comments

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  TraficLight
-//
-//  Created by Володя Рудик on 07.05.2025.
-//
 
 import UIKit
 
@@ -11,7 +5,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
