03370f10fd645693952812f4af06c1492392a248
Volodymy Rudyk <rudykvova@gmail.com>
2025-05-07 19:16:56 +0300

    Setup UI

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -1,12 +1,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var redLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn = 1
+    private let lightIsOff = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
     }
-
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
+    
+    @IBAction func startButtonTapping(_ sender: UIButton) {
+        if startButton.currentTitle == "Start" {
+            startButton.setTitle("Next", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            redLight.alpha = CGFloat(lightIsOn)
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOff
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = CGFloat(lightIsOn)
+            greenLight.alpha = lightIsOff
+            currentLight = .green
+        case . green:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = CGFloat(lightIsOn)
+            currentLight = .red
+        }
+    }
 
 }
 
+// MARK: - CurrentLight
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
